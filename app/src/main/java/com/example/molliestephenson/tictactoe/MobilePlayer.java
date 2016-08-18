1992d5e405e9c7c7e6fce8864f9e8499d9e47e39
Mollie Stephenson <mollie@8thlight.com>
2016-08-18 10:20:41 +0100

    Can play a game

---
+++ app/src/main/java/com/example/molliestephenson/tictactoe/MobilePlayer.java
@@ -0,0 +1,29 @@
+package com.example.molliestephenson.tictactoe;
+
+import ttt.Player;
+import ttt.game.Board;
+import ttt.game.Marks;
+
+public class MobilePlayer implements Player {
+
+    private Marks mark;
+
+    public MobilePlayer(Marks mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public Marks getMark() {
+        return mark;
+    }
+
+    @Override
+    public int getLocation(Board board) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public Class playerType() {
+        return getClass();
+    }
+}
